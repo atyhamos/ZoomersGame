a9c44a06d29ed6ff1200abcf86e64002106ee2cd
atyhamos <atyhalive@gmail.com>
2021-06-07 17:26:56 +0800

    Add firebase
    Slowly transitioning from PlayFab to Firebase

---
+++ ZoomersGame/Assets/Scripts/SignInResponse.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class SignInResponse
+{
+    public string localId;
+    public string idToken;
+}
